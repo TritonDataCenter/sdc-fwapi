b3812c33953e678028c013eff1f233a1b1d285b3
Rob Gulewich <robert.gulewich@joyent.com>
2012-12-03 00:27:17 -0800

    [FWAPI-4] Fetch data from new APIs

--- test/fwrule-parser.test.js
+++ test/fwrule-parser.test.js
@@ -29,15 +29,51 @@ test('tags', function(t) {
   t.deepEqual(data, expData);
 
   var rule = new FwRule({ rule: ruleTxt });
-  expData['fwuuid'] = rule.id;
+  expData['fwrule'] = rule.id;
   expData['objectclass'] = FwRule.objectclass;
-  t.deepEqual(rule.raw, expData);
+
+  t.deepEqual(rule.raw, expData, 'raw data');
 
   var serialized = rule.serialize();
   t.deepEqual(serialized, {
-    id: rule.id,
+    uuid: rule.id,
     enabled: false,
     rule: ruleTxt
+  });
+
+  t.end();
+});
+
+
+test('multiple ports', function(t) {
+  var inRule = {
+    rule: 'FROM ip 10.88.88.1 TO tag tag2 ALLOW tcp (PORT 1002 AND PORT 1052)',
+    enabled: true,
+    owner_uuid: '00000000-0000-0000-0000-000000000000'
+  };
+
+  var expRawData = {
+    action: 'allow',
+    enabled: true,
+    fromip: [ '10.88.88.1' ],
+    port: [ 1002, 1052 ],
+    protocol: 'tcp',
+    totag: [ 'tag2' ],
+    user: inRule.owner_uuid
+  };
+
+  var rule = new FwRule(inRule);
+  expRawData['fwrule'] = rule.id;
+  expRawData['objectclass'] = FwRule.objectclass;
+
+  t.deepEqual(rule.raw, expRawData, 'raw data');
+
+  t.deepEqual(rule.serialize(), {
+    enabled: true,
+    owner_uuid: inRule.owner_uuid,
+    // the canonical form of the rule has spaces around the parens:
+    rule: 'FROM ip 10.88.88.1 TO tag tag2 ALLOW tcp ( PORT 1002 AND PORT 1052 )',
+    uuid: rule.id,
   });
 
   t.end();
