68b025fde773c4c31f3fd7e1fb054f051b375694
Rob Gulewich <robert.gulewich@joyent.com>
2013-01-03 21:14:48 -0800

    [FWAPI-14] fwapi svc in maintenance after reboot
    [FWAPI-15] Do not audit log /ping

--- server.js
+++ server.js
@@ -52,33 +52,32 @@ function main() {
       res: restify.bunyan.serializers.response
     }
   });
+
   log.info('Loading config file: %s', CONFIG_FILE);
   var config = loadConfig(CONFIG_FILE);
   if (config.hasOwnProperty('logLevel')) {
     log.level(config.logLevel);
   }
 
-  fwapi.create({ config: config, log: log }, function (err, app) {
-    if (err) {
-      log.error(err, 'Error creating server');
-      process.exit(1);
-    }
-
-    server = app;
-    server.listen(function () {
-      var addr = app.server.address();
-      log.info('%s listening on <http://%s:%s>',
-        app.server.name, addr.address, addr.port);
+  try {
+    server = fwapi.create({
+      config: config,
+        log: log
     });
+  } catch (err) {
+    log.error(err, 'Error creating server');
+    process.exit(1);
+  }
+
+  server.listen(function () {
+    var addr = server.info();
+    log.info('%s listening on <http://%s:%s>',
+      addr.name, addr.address, addr.port);
   });
 
   // Try to ensure we clean up properly on exit.
   process.on('SIGINT', function _cleanup() {
     log.info('SIGINT: cleaning up');
-    if (!server) {
-      return process.exit(1);
-    }
-    log.info('Closing app');
     return server.close(function () {
       process.exit(1);
     });
